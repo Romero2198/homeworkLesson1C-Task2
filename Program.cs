33111238a7cd29a688606cdf29d01bdfe181d5fc
Yung.Energy <morskojroma@gmail.com>
2022-06-26 19:40:55 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+if (a > c)
+{
+    max = a;
+}
+Console.WriteLine("Max= " + max);
